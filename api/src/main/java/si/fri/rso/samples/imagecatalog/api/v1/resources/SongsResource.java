404ab2a7df50ce4aa1dc49ef93866cfb765128c6
Gregor <gregor.karpljuk@gmail.com>
2020-01-09 03:20:45 +0100

    addeda a media streamer + test file

--- api/src/main/java/si/fri/rso/samples/imagecatalog/api/v1/resources/SongsResource.java
+++ api/src/main/java/si/fri/rso/samples/imagecatalog/api/v1/resources/SongsResource.java
@@ -1,6 +1,7 @@
 package si.fri.rso.samples.imagecatalog.api.v1.resources;
 
 //import com.amazonaws.services.s3.AmazonS3Client;
+
 import com.google.gson.Gson;
 import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
@@ -16,12 +17,13 @@ import si.fri.rso.samples.imagecatalog.services.streaming.EventProducerImpl;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
-import java.io.IOException;
-import java.io.InputStream;
+import javax.ws.rs.core.*;
+import java.io.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletionStage;
@@ -38,7 +40,7 @@ import java.util.Formatter;
 
 public class SongsResource {
 
-    public static String SHAsum(byte[] convertme) throws NoSuchAlgorithmException{
+    public static String SHAsum(byte[] convertme) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-1");
         return byteArray2Hex(md.digest(convertme));
     }
@@ -257,5 +259,134 @@ public class SongsResource {
 
         return Response.status(Response.Status.CREATED).entity(uploadSongResponse).build();
     }
+
+    @Path("listen")
+    public class MediaResource {
+
+        final int chunk_size = 1024 * 1024; // 1MB chunks
+        private final File audio;
+
+        public MediaResource() {
+            // serve media from file system
+            String MEDIA_FILE = "testdata/music/Gryffin & Seven Lions - Need Your Love feat. Noah Kahan.mp3";
+            URL url = this.getClass().getResource(MEDIA_FILE);
+            audio = new File(url.getFile());
+        }
+
+        //A simple way to verify if the server supports range headers.
+        @HEAD
+        @Produces("audio/mp3")
+        public Response header() {
+            return Response.ok().status(206).header(HttpHeaders.CONTENT_LENGTH, audio.length()).build();
+        }
+
+        @GET
+        @Produces("audio/mp3")
+        public Response streamAudio(@HeaderParam("Range") String range) throws Exception {
+            return buildStream(audio, range);
+        }
+
+        /**
+         * Adapted from http://stackoverflow.com/questions/12768812/video-streaming-to-ipad-does-not-work-with-tapestry5/12829541#12829541
+         *
+         * @param asset Media file
+         * @param range range header
+         * @return Streaming output
+         * @throws Exception IOException if an error occurs in streaming.
+         */
+        private Response buildStream(final File asset, final String range) throws Exception {
+            // range not requested : Firefox does not send range headers
+            if (range == null) {
+                StreamingOutput streamer = output -> {
+                    try (FileChannel inputChannel = new FileInputStream(asset).getChannel(); WritableByteChannel outputChannel = Channels.newChannel(output)) {
+                        inputChannel.transferTo(0, inputChannel.size(), outputChannel);
+                    }
+                };
+                return Response.ok(streamer).status(200).header(HttpHeaders.CONTENT_LENGTH, asset.length()).build();
+            }
+
+            String[] ranges = range.split("=")[1].split("-");
+            final int from = Integer.parseInt(ranges[0]);
+
+        /*
+          Chunk media if the range upper bound is unspecified. Chrome, Opera sends "bytes=0-"
+         */
+            int to = chunk_size + from;
+            if (to >= asset.length()) {
+                to = (int) (asset.length() - 1);
+            }
+            if (ranges.length == 2) {
+                to = Integer.parseInt(ranges[1]);
+            }
+
+            final String responseRange = String.format("bytes %d-%d/%d", from, to, asset.length());
+            final RandomAccessFile raf = new RandomAccessFile(asset, "r");
+            raf.seek(from);
+
+            final int len = to - from + 1;
+            final MediaStreamer streamer = new MediaStreamer(len, raf);
+            Response.ResponseBuilder res = Response.ok(streamer)
+                    .status(Response.Status.PARTIAL_CONTENT)
+                    .header("Accept-Ranges", "bytes")
+                    .header("Content-Range", responseRange)
+                    .header(HttpHeaders.CONTENT_LENGTH, streamer.getLenth())
+                    .header(HttpHeaders.LAST_MODIFIED, new Date(asset.lastModified()));
+            return res.build();
+        }
+    }
+
+//    @GET
+//    @Produces("audio/mp3")
+//    public Response streamAudio(@HeaderParam("Range") String range) throws Exception {
+//        return buildStream("testdata/music/Gryffin & Seven Lions - Need Your Love feat. Noah Kahan.mp3", range);
+//    }
+//
+//    private Response buildStream(final File asset, final String range) throws Exception {
+//        // range not requested : Firefox, Opera, IE do not send range headers
+//        if (range == null) {
+//            StreamingOutput streamer = new StreamingOutput() {
+//                @Override
+//                public void write(final OutputStream output) throws IOException, WebApplicationException {
+//
+//                    final FileChannel inputChannel = new FileInputStream(asset).getChannel();
+//                    final WritableByteChannel outputChannel = Channels.newChannel(output);
+//                    try {
+//                        inputChannel.transferTo(0, inputChannel.size(), outputChannel);
+//                    } finally {
+//                        // closing the channels
+//                        inputChannel.close();
+//                        outputChannel.close();
+//                    }
+//                }
+//            };
+//            return Response.ok(streamer).header(HttpHeaders.CONTENT_LENGTH, asset.length()).build();
+//        }
+//
+//        String[] ranges = range.split("=")[1].split("-");
+//        final int from = Integer.parseInt(ranges[0]);
+//        /**
+//         * Chunk media if the range upper bound is unspecified. Chrome sends "bytes=0-"
+//         */
+//        int to = 512 * 512 + from;
+//        if (to >= asset.length()) {
+//            to = (int) (asset.length() - 1);
+//        }
+//        if (ranges.length == 2) {
+//            to = Integer.parseInt(ranges[1]);
+//        }
+//
+//        final String responseRange = String.format("bytes %d-%d/%d", from, to, asset.length());
+//        final RandomAccessFile raf = new RandomAccessFile(asset, "r");
+//        raf.seek(from);
+//
+//        final int len = to - from + 1;
+//        final MediaStreamer streamer = new MediaStreamer(len, raf);
+//        Response.ResponseBuilder res = Response.status(Response.Status.PARTIAL_CONTENT).entity(streamer)
+//                .header("Accept-Ranges", "bytes")
+//                .header("Content-Range", responseRange)
+//                .header(HttpHeaders.CONTENT_LENGTH, streamer.getLenth())
+//                .header(HttpHeaders.LAST_MODIFIED, new Date(asset.lastModified()));
+//        return res.build();
+//    }
 
 }
