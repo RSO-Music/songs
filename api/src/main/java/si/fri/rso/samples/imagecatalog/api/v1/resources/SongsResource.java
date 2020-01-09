f86fcfd64e7b70caaa90023fc7e9cfa9eccf6e65
Gregor <gregor.karpljuk@gmail.com>
2020-01-09 03:50:20 +0100

    tests

--- api/src/main/java/si/fri/rso/samples/imagecatalog/api/v1/resources/SongsResource.java
+++ api/src/main/java/si/fri/rso/samples/imagecatalog/api/v1/resources/SongsResource.java
@@ -184,6 +184,15 @@ public class SongsResource {
 
     }
 
+    @GET
+    @Path("/test")
+    public Response test(String imageMetadataId) {
+
+        UploadSongResponse uploadSongResponse = new UploadSongResponse();
+        uploadSongResponse.setMessage("test successful");
+        return Response.status(Response.Status.CREATED).entity(uploadSongResponse).build();
+    }
+
     @DELETE
     @Path("{imageMetadataId}")
     public Response deleteImageMetadata(@PathParam("imageMetadataId") Integer imageMetadataId) {
@@ -270,69 +279,69 @@ public class SongsResource {
 //            audio = new File(url.getFile());
 //        }
 
-        //A simple way to verify if the server supports range headers.
+    //A simple way to verify if the server supports range headers.
 //        @HEAD
 //        @Produces("audio/mp3")
 //        public Response header() {
 //            return Response.ok().status(206).header(HttpHeaders.CONTENT_LENGTH, audio.length()).build();
 //        }
 
-        @GET
-        @Path("/listen")
-        @Produces("audio/mp3")
-        public Response streamAudio(@HeaderParam("Range") String range) throws Exception {
-            // serve media from file system
-            String MEDIA_FILE = "testdata/music/Gryffin & Seven Lions - Need Your Love feat. Noah Kahan.mp3";
-            URL url = this.getClass().getResource(MEDIA_FILE);
-            audio = new File(url.getFile());
-            return buildStream(audio, range);
+    @GET
+    @Path("/listen")
+    @Produces("audio/mp3")
+    public Response streamAudio(@HeaderParam("Range") String range) throws Exception {
+        // serve media from file system
+        String MEDIA_FILE = "testdata/music/Gryffin & Seven Lions - Need Your Love feat. Noah Kahan.mp3";
+        URL url = this.getClass().getResource(MEDIA_FILE);
+        audio = new File(url.getFile());
+        return buildStream(audio, range);
+    }
+
+    /**
+     * Adapted from http://stackoverflow.com/questions/12768812/video-streaming-to-ipad-does-not-work-with-tapestry5/12829541#12829541
+     *
+     * @param asset Media file
+     * @param range range header
+     * @return Streaming output
+     * @throws Exception IOException if an error occurs in streaming.
+     */
+    private Response buildStream(final File asset, final String range) throws Exception {
+        // range not requested : Firefox does not send range headers
+        if (range == null) {
+            StreamingOutput streamer = output -> {
+                try (FileChannel inputChannel = new FileInputStream(asset).getChannel(); WritableByteChannel outputChannel = Channels.newChannel(output)) {
+                    inputChannel.transferTo(0, inputChannel.size(), outputChannel);
+                }
+            };
+            return Response.ok(streamer).status(200).header(HttpHeaders.CONTENT_LENGTH, asset.length()).build();
         }
 
-        /**
-         * Adapted from http://stackoverflow.com/questions/12768812/video-streaming-to-ipad-does-not-work-with-tapestry5/12829541#12829541
-         *
-         * @param asset Media file
-         * @param range range header
-         * @return Streaming output
-         * @throws Exception IOException if an error occurs in streaming.
-         */
-        private Response buildStream(final File asset, final String range) throws Exception {
-            // range not requested : Firefox does not send range headers
-            if (range == null) {
-                StreamingOutput streamer = output -> {
-                    try (FileChannel inputChannel = new FileInputStream(asset).getChannel(); WritableByteChannel outputChannel = Channels.newChannel(output)) {
-                        inputChannel.transferTo(0, inputChannel.size(), outputChannel);
-                    }
-                };
-                return Response.ok(streamer).status(200).header(HttpHeaders.CONTENT_LENGTH, asset.length()).build();
-            }
-
-            String[] ranges = range.split("=")[1].split("-");
-            final int from = Integer.parseInt(ranges[0]);
+        String[] ranges = range.split("=")[1].split("-");
+        final int from = Integer.parseInt(ranges[0]);
 
         /*
           Chunk media if the range upper bound is unspecified. Chrome, Opera sends "bytes=0-"
          */
-            int to = chunk_size + from;
-            if (to >= asset.length()) {
-                to = (int) (asset.length() - 1);
-            }
-            if (ranges.length == 2) {
-                to = Integer.parseInt(ranges[1]);
-            }
-
-            final String responseRange = String.format("bytes %d-%d/%d", from, to, asset.length());
-            final RandomAccessFile raf = new RandomAccessFile(asset, "r");
-            raf.seek(from);
-
-            final int len = to - from + 1;
-            final MediaStreamer streamer = new MediaStreamer(len, raf);
-            Response.ResponseBuilder res = Response.ok(streamer)
-                    .status(Response.Status.PARTIAL_CONTENT)
-                    .header("Accept-Ranges", "bytes")
-                    .header("Content-Range", responseRange)
-                    .header(HttpHeaders.CONTENT_LENGTH, streamer.getLenth())
-                    .header(HttpHeaders.LAST_MODIFIED, new Date(asset.lastModified()));
-            return res.build();
+        int to = chunk_size + from;
+        if (to >= asset.length()) {
+            to = (int) (asset.length() - 1);
         }
+        if (ranges.length == 2) {
+            to = Integer.parseInt(ranges[1]);
+        }
+
+        final String responseRange = String.format("bytes %d-%d/%d", from, to, asset.length());
+        final RandomAccessFile raf = new RandomAccessFile(asset, "r");
+        raf.seek(from);
+
+        final int len = to - from + 1;
+        final MediaStreamer streamer = new MediaStreamer(len, raf);
+        Response.ResponseBuilder res = Response.ok(streamer)
+                .status(Response.Status.PARTIAL_CONTENT)
+                .header("Accept-Ranges", "bytes")
+                .header("Content-Range", responseRange)
+                .header(HttpHeaders.CONTENT_LENGTH, streamer.getLenth())
+                .header(HttpHeaders.LAST_MODIFIED, new Date(asset.lastModified()));
+        return res.build();
+    }
 }
